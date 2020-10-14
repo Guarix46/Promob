e3a982e0a7bcf84b6f5c8dccab95898296c57f83
Guarix46 <irvanguari9@gmail.com>
2020-10-14 22:56:29 +0700

    Menambahkan test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+A = int(input("Masukkan Nilai A : "))
+B = int(input("Masukkan Nilai B : "))
+C = A,B
+
+def myfunction(A , B):
+    D = A * B
+    print("Hasil Kali dua Bilangan adalah ", D)
+
+#Pemanggilan 2 Bilangan
+print("Nilai dua bilangan tsb adalah : ", C)
+
+#Pemanggilan Fungsi
+myfunction(A , B)
